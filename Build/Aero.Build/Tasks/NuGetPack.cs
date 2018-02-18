a6c0a4dedba5819285a5f577391de5f30af9a2da
Adam Salvo <adam.salvo@trainerroad.com>
2018-02-17 21:44:21 -0600

    Pack everything

--- Build/Aero.Build/Tasks/NuGetPack.cs
+++ Build/Aero.Build/Tasks/NuGetPack.cs
@@ -13,19 +13,38 @@ namespace Aero.Build.Tasks
         public override void Run(Context context)
         {
             var appVersion = context.Argument<string>("AppVersion");
-            var projectToPack = context.Argument<string>("Project");
-
             var dotNetCore = context.ServiceProvider.GetService<IDotNetCoreCupCake>();
 
-            var path = new FilePath($"{context.ProjectsPath}/{projectToPack}/{projectToPack}.csproj");
-            dotNetCore.Pack(path.FullPath, new DotNetCorePackSettings
+            var settings = new DotNetCorePackSettings
             {
                 Configuration = context.Configuration,
                 IncludeSymbols = true,
                 NoBuild = true,
                 ArgumentCustomization = args => args
                     .Append($"/p:Version={appVersion}")
-            });
+            };
+            
+            PackAero(context, dotNetCore, settings);
+            PackAeroAzure(context, dotNetCore, settings);
+            PackAeroCake(context, dotNetCore, settings);
+        }
+
+        private void PackAero(Context context, IDotNetCoreCupCake dotNetCore, DotNetCorePackSettings defaultSettings)
+        {
+            var path = new FilePath($"{context.ProjectsPath}/Aero/Aero.csproj");
+            dotNetCore.Pack(path.FullPath, defaultSettings);
+        }
+
+        private void PackAeroAzure(Context context, IDotNetCoreCupCake dotNetCore, DotNetCorePackSettings defaultSettings)
+        {
+            var path = new FilePath($"{context.ProjectsPath}/Aero.Azure/Aero.Azure.csproj");
+            dotNetCore.Pack(path.FullPath, defaultSettings);
+        }
+
+        private void PackAeroCake(Context context, IDotNetCoreCupCake dotNetCore, DotNetCorePackSettings defaultSettings)
+        {
+            var path = new FilePath($"{context.ProjectsPath}/Aero.Cake/Aero.Cake.csproj");
+            dotNetCore.Pack(path.FullPath, defaultSettings);
         }
     }
 }
